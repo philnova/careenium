8218aff1c9879d0aedd830f4ffff8ffc0eb7287a
Phil Nova <pnova8@gmail.com>
2017-02-03 14:06:45 -0800

    render canvas on main page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
 
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    return render_template('main.html',
+                            development=True,)
