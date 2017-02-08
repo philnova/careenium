86b02014f99001eff1e0ae3452fda4d95f361f41
Phil Nova <pnova8@gmail.com>
2017-02-08 14:00:23 -0800

    add collision detection for walls

--- app/static/simulation_classes.js
+++ app/static/simulation_classes.js
@@ -3,10 +3,13 @@ function Position (x, y) {
     this.y = y;
 };
 
-function Vector (pos, magnitude) {
-    this.pos = pos; // use a Position object
-    this.magnitude = magnitude;
-};
+Position.prototype.flipX = function() {
+    this.x = -this.x;
+}
+
+Position.prototype.flipY = function() {
+    this.y = -this.y;
+}
 
 function Simm (radius, mass, pos, velocity) {
     this.radius = radius;
@@ -15,9 +18,21 @@ function Simm (radius, mass, pos, velocity) {
     this.velocity = velocity;
 };
 
+Simm.prototype.checkXWallCollision = function(min, max) {
+    if (this.pos.x <= min || this.pos.x >= max) {
+        this.velocity.flipX();
+    }
+}
+
+Simm.prototype.checkYWallCollision = function(min, max) {
+    if (this.pos.y <= min || this.pos.y >= max) {
+        this.velocity.flipY();
+    }
+}
+
 Simm.prototype.move = function() {
-    this.pos.x++;
-    this.pos.y++;
+    this.pos.x = this.pos.x + this.velocity.x;
+    this.pos.y = this.pos.y + this.velocity.y;
 };
 
 function Simulation (simm_array, canvas, ctx) {
@@ -28,8 +43,16 @@ function Simulation (simm_array, canvas, ctx) {
 
 Simulation.prototype.advanceOne = function() {
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.checkWallCollision();
     for (var i=0; i<this.simm_array.length; i++) {
-        this.simm_array[i].move(1.0);
+        this.simm_array[i].move();
     };
     draw_objects_on_canvas(this.ctx, this.canvas, this.simm_array);
+};
+
+Simulation.prototype.checkWallCollision = function() {
+    for (var i=0; i<this.simm_array.length; i++) {
+        this.simm_array[i].checkXWallCollision(0, canvas.width);
+        this.simm_array[i].checkYWallCollision(0, canvas.height);
+    }
 };
